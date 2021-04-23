e82e62cc56acad6bafd3fb1505da33cca4052b2b
Claudio Inacio <inacio.unipar@gmail.com>
2021-04-22 22:38:31 -0300

    Primeira classe: Posicao

--- src/aplicacao/projeto.java
+++ src/aplicacao/projeto.java
@@ -1,12 +1,22 @@
 package aplicacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import jogoTabuleiro.Posicao;
+
 public class projeto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println(" hello world");
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
 			
+		Posicao posi = new Posicao(3, 5);
+		System.out.print(posi);
+			
+		
+		sc.close();
 	}
 
 }
